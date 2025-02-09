f4cde546b85cfdb2a2adbcf2dcd661f6e2ad3184
Hanan <workinghuman123@gmail.com>
2025-02-09 17:51:59 +0530

    changed api2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({ message: "This is fresh changed message from The Backend" });
+    res.json({ message: "This is fresh changed message from The Backend 😁" });
 });
 
 app.listen(PORT, () => {
